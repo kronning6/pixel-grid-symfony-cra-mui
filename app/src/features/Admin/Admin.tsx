3218059618c1e03eea379446a1853961ff2e6681
Kyle Ronning <kronning@kipsu.com>
2022-10-17 20:43:25 -0500

    Update admin page

--- app/src/features/Admin/Admin.tsx
+++ app/src/features/Admin/Admin.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Box from '../../components/mui/Box/Box';
+import Button from '../../components/mui/Button/Button';
+import Stack from '../../components/mui/Stack/Stack';
+import Typography from '../../components/mui/Typography/Typography';
 
 const Admin = (): JSX.Element => {
+  const navigate = useNavigate();
   return (
     <Box
       sx={{
@@ -11,9 +16,32 @@ const Admin = (): JSX.Element => {
         width: '100%',
       }}
     >
-      <Box>Admin - Add Pixels</Box>
-      <Box>Admin - Add User</Box>
-      <Box>Admin - Delete User</Box>
+      <Box
+        sx={{
+          marginTop: '40px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '400px',
+        }}
+      >
+        <Box sx={{ textAlign: 'center' }}>
+          <Typography variant="h2">Admin</Typography>
+        </Box>
+        <Box mt={5} textAlign="center">
+          TODO - Add Pixels
+        </Box>
+        <Box mt={2} textAlign="center">
+          TODO - Add Pixels
+        </Box>
+        <Box mt={2} textAlign="center">
+          TODO - Delete User
+        </Box>
+        <Stack mt={5} spacing={1}>
+          <Button variant="outlined" color="primary" size="large" onClick={() => navigate('/')}>
+            Enter the Grid
+          </Button>
+        </Stack>
+      </Box>
     </Box>
   );
 };
